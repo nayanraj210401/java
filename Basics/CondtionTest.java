06cc44295b7e9132df7827401cf8baff27d19e16
nayanraj210401 <46132153+nayanraj210401@users.noreply.github.com>
2019-06-22 17:21:31 +0530

    Update CondtionTest.java

--- Basics/CondtionTest.java
+++ Basics/CondtionTest.java
@@ -8,18 +8,18 @@ public class CondtionTest {
 
 
 
-//        Scanner sc =new Scanner(System.in);
-//        System.out.println("Enter the year");
-//       int N =sc.nextInt();
-//        if(N%4==0 && N%100 !=0 || N%400==0){
-//            System.out.println("LeapYear");
-//        }
-//        else
-//        {
-//            System.out.println("Not an LeapYear");
-//        }
-//
-//        System.out.println((N%4 == 0)? "LeapYear":"NOTLeapYear");
+       Scanner sc =new Scanner(System.in);
+       System.out.println("Enter the year");
+      int N =sc.nextInt();
+       if(N%4==0 && N%100 !=0 || N%400==0){
+           System.out.println("LeapYear");
+       }
+       else
+       {
+           System.out.println("Not an LeapYear");
+       }
+
+       System.out.println((N%4 == 0)? "LeapYear":"NOTLeapYear");
 
 
 
